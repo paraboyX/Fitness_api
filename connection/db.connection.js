817cb7e3595499260c3663e8ed4388a90b36cac2
Ranit Naha <naharanit123@gmail.com>
2024-03-12 22:04:10 +0530

    bug/GOD commit

--- connection/db.connection.js
+++ connection/db.connection.js
@@ -1,15 +1,11 @@
-require("dotenv").config();
+require("dotenv").config({ path: "../.env" });
 const mongoose = require("mongoose");
 const MONGO_URL = process.env.MONGO_URL;
 
 const connection = async () => {
   try {
-    await mongoose
-      .connect(MONGO_URL)
-      .then(() => {
-        console.log("DB connection established");
-      })
-      .catch((err) => console.log(err));
+    await mongoose.connect("mongodb://localhost:27017/Nutrition");
+    console.log("DB connection established");
   } catch (error) {
     console.error("MongoDB connection error:", error);
   }
