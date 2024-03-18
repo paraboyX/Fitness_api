163988434c6081af72165e59bb31a20296a56ae2
Ranit Naha <naharanit123@gmail.com>
2024-03-19 01:31:36 +0530

    creat/foodtracking model

--- model/foodtraking.model.js
+++ model/foodtraking.model.js
@@ -1,1 +1,22 @@
- 
+const mongoose = require("mongoose");
+
+const foodTracking = mongoose.Schema(
+  {
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: "user",
+    },
+    food: {
+      type: Schema.Types.ObjectId,
+      ref: "food",
+    },
+    date: {
+      type: Date,
+      default: new Date(),
+    },
+  },
+  { timestamps: true }
+);
+
+const foodTrackingModel = mongoose.model("tracking", foodTracking);
+module.exports = foodTrackingModel;
